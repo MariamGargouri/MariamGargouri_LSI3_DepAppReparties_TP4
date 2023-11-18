461d7ae6a2005066a7437eeaf0e0fbe2b504abea
MariamGargouri <148018137+MariamGargouri@users.noreply.github.com>
2023-11-18 23:09:15 +0100

    Update Client.java

--- src/Activite4_3/Client.java
+++ src/Activite4_3/Client.java
@@ -10,21 +10,30 @@ public class Client {
     public static void main(String[] args) {
         try {
         	
-        	DatagramSocket clientSocket = new DatagramSocket();
-            InetAddress serverAddress = InetAddress.getByName("localhost");
+        	DatagramSocket clientSocket = new DatagramSocket();// Crée un socket client pour la communication UDP
+
+            InetAddress serverAddress = InetAddress.getByName("localhost");// Obtient l'adresse IP du serveur (localhost dans cet exemple)
             
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Entrez votre nom d'utilisateur: ");
+            Scanner scanner = new Scanner(System.in);// Crée un scanner pour la saisie utilisateur
+            System.out.print("Entrez votre nom d'utilisateur: ");// Demande à l'utilisateur de saisir son nom d'utilisateur
             String username = scanner.nextLine();
-            System.out.print("Message: ");
+            System.out.print("Message: ");// Demande à l'utilisateur de saisir un message
             String message = scanner.nextLine();
 
-            // Prépare le message à envoyer au serveur
+            // Préparation du message qui sera  envoyé au serveur
             String fullMessage = "[" + username + "]: " + message;
             byte[] sendData = fullMessage.getBytes();
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, PORT);
           
-            clientSocket.send(sendPacket);
+            clientSocket.send(sendPacket);// Envoie le paquet au serveur
+            
+           // réception des messages d'autres clients
+         	DatagramPacket receivePacket= new DatagramPacket(buffer,buffer.length);
+         	clientSocket.receive(receivePacket); // la reception du datagram envoyé par le serveur
+         	String messages = new String(receivePacket.getData(),0,receivePacket.getLength());
+         	System.out.println(messages);
+         	
+         	clientSocket.close();// fermeture du socket
             } catch (IOException e) {e.printStackTrace();}
     }
 }
